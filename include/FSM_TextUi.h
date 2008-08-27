bf33416717cd62811408a0eb0a13564dcd4d3a32
Julien Thevenon <julien_thevenon@yahoo.fr>
2008-08-28 00:01:04 +0200

    Update to remove namespace from headers

--- include/FSM_TextUi.h
+++ include/FSM_TextUi.h
@@ -1,28 +1,27 @@
-#ifndef _FSM_TEXT_UI_H_
-#define _FSM_TEXT_UI_H_
+#ifndef FSM_TEXT_UI_H
+#define FSM_TEXT_UI_H
 
 #include "framework_UI_if.h"
 
 #include <vector>
-using namespace std;
 
 class framework_controler;
 
 class FSM_TextUi: public framework_UI_if
 {
-	public:
-		FSM_TextUi(void);
-		void run(void);
+ public:
+  FSM_TextUi(void);
+  void run(void);
 
-		// Methods inherited from framework_UI_if
-		void displayLoadedFsm(vector<string> *p_fsm_list);
-		void displayLoadedFsmUi(vector<string> *p_fsm_ui_list);
-		void displayLoadedAlgorithm(vector<string> *p_algorithm_list);
-		void displayWarningMessage(string p_message);
-		void displayErrorMessage(string p_message);
+  // Methods inherited from framework_UI_if
+  void displayLoadedFsm(std::vector<std::string> *p_fsm_list);
+  void displayLoadedFsmUi(std::vector<std::string> *p_fsm_ui_list);
+  void displayLoadedAlgorithm(std::vector<std::string> *p_algorithm_list);
+  void displayWarningMessage(std::string p_message);
+  void displayErrorMessage(std::string p_message);
 
-	private:
-		unsigned int displayMenu(void);
-		framework_controler *m_framework;
+ private:
+  unsigned int displayMenu(void);
+  framework_controler *m_framework;
 };
-#endif
+#endif /* FSM_TEXT_UI_H */
